bbf6b96e93a19f147b038beed6c2b34c2ecca12a
noi4eg <l2d@yandex.ru>
2021-11-26 13:11:47 +0500

    task8 rec

--- task8.py
+++ task8.py
@@ -2,8 +2,32 @@
 # в которой первые два числа равны 0 и 1, а каждое последующее число равно сумме двух предыдущих чисел.
 
 def fib(n):
-    pass
+    n1 = 0
+    n2 = 1
+    if 0 == n:
+        sum = 0
+    elif 1 == n:
+        sum = 1
+    else:
+        for elem in range (1,n):
+            sum = n1 + n2
+            n1 = n2
+            n2 = sum
+            
+    return sum
 
 
-n = int(input())
+def fib_rec(n):
+    if n in {0, 1}:
+        return n
+    return fib_rec(n-1) + fib_rec(n-2)
+
+try:
+    n = int(input().strip())
+except ValueError:
+    n = 5
+
 print (fib(n))
+print (fib_rec(n))
+
+
